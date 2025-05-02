c81a78a6e8f484da0fc7929dffaf3d2a00c555e6
Gautam696969 <palgautam69@gmail.com>
2025-05-02 12:25:20 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
 // Replace 'voice-AI1' with your actual repo name
-export default defineConfig({
+export default ({
   plugins: [react()],
   base: '/Health-Software/' // important for GitHub Pages
 })
