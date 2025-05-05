3ad3f229b9f3a7a5b9c578d9876f583ed44670ec
Gautam696969 <palgautam69@gmail.com>
2025-05-05 10:47:05 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // Replace 'voice-AI1' with your actual repo name
 export default ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Health-Software/' // important for GitHub Pages
 })
